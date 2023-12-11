e96c17280161ee945ac6ffa88260b56097534fe7
Jon Weisse <jonathonweisse@gmail.com>
2023-12-11 12:18:24 -0600

    day 9-2

--- 2023/day-9/script.js
+++ 2023/day-9/script.js
@@ -9,26 +9,17 @@ function findSumOfAllExtrapolations(input) {
 }
 
 function findNextNumberInSequence(sequence){
-    // console.log(sequence)
-    let values = sequence.split(' ')
+    let values = sequence.split(' ').reverse() // remove .reverse for part 1
     let temp = [values]
-    // console.log(temp)
 
     while(temp[temp.length-1].reduce((total,x)=>{return total+x},0) != 0 || temp[temp.length-1].length>1) {
         let current = temp[temp.length-1]
-        // console.log(current)
         let next = []
         current.forEach((x,i) => {
             if(!isNaN(current[i+1])) next.push(current[i+1] - x)
         })
-    // console.log(next)
         temp.push(next)
     }
-
-
-    // temp.forEach((line,index) => {
-    //     console.log(`${index}: ${line}`)
-    // })
 
     let lastNumbers = temp.map(arr => arr[arr.length-1])
 
