fc8252175bd3958b495b1c4b74d2e3f6f15cbea8
Jon Weisse <jonathonweisse@gmail.com>
2023-12-15 12:21:53 -0600

    day 13 part 2

--- 2023/day-13/script.js
+++ 2023/day-13/script.js
@@ -6,18 +6,18 @@ function findRowMatch(rows) {
     let lastRow = rows.length - 1
     for(let i=1; i<lastRow;i++) {
         let subArray
-        if(rows[0] == rows[i]) {
+        if(isFuzzyMirror([rows[0], rows[i]],1,false)) {
             subArray = rows.slice(0,i+1)
-            if(isMirroredArray(subArray)) {
+            if(isFuzzyMirror(subArray)) {
                 if(subArray.length > largest) {
                     largest = subArray.length
                     value = subArray.length/2
                 }
             }
         }
-        if(rows[lastRow] == rows[lastRow - i]) {
+        if(isFuzzyMirror([rows[lastRow],rows[lastRow - i]],1,false)) {
             subArray = rows.slice(lastRow - i)
-            if(isMirroredArray(subArray)) {
+            if(isFuzzyMirror(subArray)) {
                 if(subArray.length > largest) {
                     largest = subArray.length
                     value = (lastRow - i) + (i+1)/2
@@ -39,6 +39,29 @@ function isMirroredArray(arr) {
     return firstHalf == secondHalf
 }
 
+function isFuzzyMirror(arr,fuzz=1,strict=true) {
+    if(arr.length % 2 == 1) return false
+    let strikes = 0
+    let isMirror = true
+    let half = arr.length / 2
+    let firstHalf = arr.slice(0,half).reverse().map(line=>line.split('')).flat()
+    let secondHalf = arr.slice(half).map(line=>line.split('')).flat()
+
+    for(let i=0; i<firstHalf.length;i++) {
+        if(firstHalf[i] !== secondHalf[i]) {
+            strikes++
+            if(strikes > fuzz) {
+                isMirror = false
+                i += firstHalf.length
+            }
+        }
+    }
+    if(strict) {
+        isMirror = (strikes == fuzz && isMirror) ? true : false
+    }
+    return isMirror
+}
+
 function flipColumnsToRows(rows) {
     const flippedData = Array.apply(null, Array(rows[0].length)).map(()=>'')
     rows.forEach(line => {
@@ -57,6 +80,8 @@ function summarizeReflections(data) {
         let rowMatch = findRowMatch(rows)
         let columns = flipColumnsToRows(rows)
         let colMatch = findRowMatch(columns)
+
+        // console.log(`${index}: ${rowMatch.size}-${rowMatch.val} ${colMatch.size}-${colMatch.val}`)
         
         if(rowMatch.size > 0) {
             total += rowMatch.val * 100
