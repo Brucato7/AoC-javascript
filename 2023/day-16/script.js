617c3240211ea70d624d110c1f8fb5c0d0c75ec5
Jon Weisse <jonathonweisse@gmail.com>
2023-12-18 15:37:51 -0600

    day 16 part 2

--- 2023/day-16/script.js
+++ 2023/day-16/script.js
@@ -1,10 +1,8 @@
 const fs = require('fs')
 const { convertTxtToArray } = require('../../utilities/read-input')
 
-const activatedNodes = []
-
 // [0,0], 'right', data
-function followBeam(prevNode,dir) {
+function followBeam(prevNode,dir,activatedNodes=[],map) {
     // console.log(node,dir)
     let [newY,newX] = prevNode
     if(dir == 'right') newX++
@@ -13,30 +11,30 @@ function followBeam(prevNode,dir) {
     else if(dir == 'down') newY++
     let total = 0
 
-    if(newX < 0 || newY < 0 || newX >= data[0].length || newY >= data.length) return 0
+    if(newX < 0 || newY < 0 || newX >= map[0].length || newY >= map.length) return 0
     let stringNode = newY + '-' + newX
     if(!activatedNodes.includes(stringNode)) {
         activatedNodes.push(stringNode)
         total++
     }
-    const char = data[newY][newX]
+    const char = map[newY][newX]
     let currNode = [newY,newX]
     if(char == '$') {
         return total
     } else if(char == '|' && ['right','left'].includes(dir)) {
-        data[newY][newX] = '$'
-        total += followBeam(currNode,'up')
-        total += followBeam(currNode,'down')
+        map[newY][newX] = '$'
+        total += followBeam(currNode,'up',activatedNodes,map)
+        total += followBeam(currNode,'down',activatedNodes,map)
     } else if(char == '-' && ['up','down'].includes(dir)) {
-        data[newY][newX] = '$'
-        total += followBeam(currNode,'right')
-        total += followBeam(currNode,'left')
+        map[newY][newX] = '$'
+        total += followBeam(currNode,'right',activatedNodes,map)
+        total += followBeam(currNode,'left',activatedNodes,map)
     } else if(char == '\\') {
-        total += followBeam(currNode,backSlashMap[dir])
+        total += followBeam(currNode,backSlashMap[dir],activatedNodes,map)
     } else if(char == '/') {
-        total += followBeam(currNode,forwardSlashMap[dir])
+        total += followBeam(currNode,forwardSlashMap[dir],activatedNodes,map)
     } else {
-        total += followBeam(currNode,dir)
+        total += followBeam(currNode,dir,activatedNodes,map)
     }
 
     return total
@@ -57,8 +55,27 @@ const backSlashMap = {
 
 const data = convertTxtToArray('./input.txt').map(line=>line.split(''))
 
-let result = 1
+let length = data[0].length
+let height = data[0].length
 
-result += followBeam([0,-1],'right')
+const results = []
 
-console.log(activatedNodes.length)
+for(let l=0;l<length;l++) {
+    let map = JSON.parse(JSON.stringify(data))
+    results.push(followBeam([-1,l],'down',[],map))
+    results.push(followBeam([height,l],'up',[],map))
+}
+for(let h=0;h<height;h++) {
+    let map = JSON.parse(JSON.stringify(data))
+    results.push(followBeam([h,-1],'right',[],map))
+    results.push(followBeam([h,length],'left',[],map))
+}
+
+console.log(results)
+console.log(results.sort((a,b)=>b-a)[0])
+
+// let result = 0
+// result += followBeam([0,-1],'right',[])
+
+// console.log(result)
+// console.log(activatedNodes.length)
